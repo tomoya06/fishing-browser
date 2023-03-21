5502c04c442af7c929e48a1eacfce0e6ccb76f0f
ITIStomoya06 <35499042+tomoya06@users.noreply.github.com>
2023-03-21 23:28:39 +0800

    feat: game on

---
+++ src/background/fishing.ts
@@ -0,0 +1,78 @@
+console.info('chrome-ext template-react-ts background script');
+
+interface ClosedTabInfo {
+  browserDuration: number;
+  openTimes: number;
+}
+
+interface OpenedTabInfo {
+  activeTs: number;
+  isActive: boolean;
+  browserDuration: number;
+}
+
+const CLOSED_TABS = new Map<string, ClosedTabInfo>();
+const OPENED_TABS = new Map<string, OpenedTabInfo>();
+
+function getCurTimestamp() {
+  return new Date().getTime();
+}
+
+export function goFishing(tabid: string) {
+  const newActiveTime = getCurTimestamp();
+  const prevInfo = OPENED_TABS.get(tabid);
+  if (prevInfo) {
+    prevInfo.activeTs = newActiveTime;
+    OPENED_TABS.set(tabid, prevInfo);
+  } else {
+    OPENED_TABS.set(tabid, {
+      activeTs: newActiveTime,
+      browserDuration: 0,
+      isActive: true,
+    });
+  }
+}
+
+export function pauseFishing(tabid: string) {
+  const inactiveTime = getCurTimestamp();
+  const prevInfo = OPENED_TABS.get(tabid);
+  if (!prevInfo) {
+    return;
+  }
+
+  const curDuration = inactiveTime - prevInfo.activeTs;
+
+  prevInfo.browserDuration += curDuration;
+  prevInfo.isActive = false;
+  OPENED_TABS.set(tabid, prevInfo);
+}
+
+export function finishFishing(tabid: string) {
+  const closedTime = getCurTimestamp();
+  const prevInfo = OPENED_TABS.get(tabid);
+  if (!prevInfo) {
+    return;
+  }
+
+  const lastDuration = prevInfo.isActive ? closedTime - prevInfo.activeTs : 0;
+  prevInfo.browserDuration += lastDuration;
+
+  const prevClosedInfo = CLOSED_TABS.get(tabid);
+  if (!prevClosedInfo) {
+    CLOSED_TABS.set(tabid, {
+      browserDuration: prevInfo.browserDuration,
+      openTimes: 1,
+    });
+  } else {
+    CLOSED_TABS.set(tabid, {
+      browserDuration: prevClosedInfo.browserDuration + prevInfo.browserDuration,
+      openTimes: prevClosedInfo.openTimes + 1,
+    });
+  }
+
+  OPENED_TABS.delete(tabid);
+}
+
+export function genTabId(tabid: number, tabinfo?: chrome.tabs.Tab): string {
+  return `${tabid}`;
+}
---
+++ src/background/index.ts
@@ -0,0 +1,54 @@
+import { finishFishing, genTabId, goFishing, pauseFishing } from './fishing';
+
+console.info('chrome-ext template-react-ts background script');
+
+function doGoFishing(tabid: number) {
+  goFishing(genTabId(tabid));
+}
+
+function doPauseFishing(tabid: number) {
+  pauseFishing(genTabId(tabid));
+}
+
+function doFinishFishing(tabid: number) {
+  finishFishing(genTabId(tabid));
+}
+
+const CREATED_TABS = new Set<number>();
+let lastActiveTabInfo = 0;
+
+chrome.tabs.onCreated.addListener((tab) => {
+  if (!tab.id || tab.id === chrome.tabs.TAB_ID_NONE) {
+    return;
+  }
+  CREATED_TABS.add(tab.id);
+});
+
+chrome.tabs.onUpdated.addListener((tabid, changeInfo, tab) => {
+  if (!CREATED_TABS.has(tabid)) {
+    return;
+  }
+
+  if (changeInfo.status === 'complete' && tab.active) {
+    CREATED_TABS.delete(tabid);
+    doGoFishing(tabid);
+
+    return;
+  }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  if (lastActiveTabInfo) {
+    doPauseFishing(lastActiveTabInfo);
+  }
+  doGoFishing(activeInfo.tabId);
+  lastActiveTabInfo = activeInfo.tabId;
+});
+
+chrome.tabs.onRemoved.addListener((tabid, removeInfo) => {
+  doFinishFishing(tabid);
+});
+
+// FIXME: chrome app lose focus?
+
+export {};
